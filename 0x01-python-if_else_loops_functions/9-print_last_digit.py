7a54ef4f985e220ac7e2e5712f0215cf7977e224
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-12-10 07:30:26 -0800

    task 9 commit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    for n in number:
-        print(abs(n) % 10, end="")
-        return(abs(n) % 10)
+        print(abs(number) % 10, end="")
+        return(abs(number) % 10)
