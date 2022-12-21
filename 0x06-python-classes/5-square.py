3021bd0a6bcd0a6cfecd0822aa7215f482674691
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-12-21 13:52:24 -0800

    square5 task commit

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -2,6 +2,7 @@
 
 """Define a class Square."""
 
+
 class Square:
     """Represent a square."""
     def __init__(self, size):
@@ -10,20 +11,24 @@ class Square:
             size (int): The size of the new square.
         """
         self.size = size
+
     @property
     def size(self):
         """Retrieve size"""
         return (self.__size)
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
+
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
+
     def my_print(self):
         """Print the square with the # character."""
         for i in range(0, self.__size):
