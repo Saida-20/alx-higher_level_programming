618430743ba658699a50afd9eb4e4415b70ac2cd
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-12-21 13:32:30 -0800

    python classes commit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -18,7 +18,7 @@ class Square:
         return (self.__size)
     @size.setter
     def size(self, value):
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
