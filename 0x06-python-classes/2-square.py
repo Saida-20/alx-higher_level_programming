e69a5954d52f19bc9621b0fc1fa963bba85453d1
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-12-21 12:53:44 -0800

    classes tasks commit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 """Define a class Square."""
+
+
 class Square:
     """Represent a square."""
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """initialize a new square.
         Args:
             size (int): The size of the new square.
