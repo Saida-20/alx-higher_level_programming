2531d5a81583335bc6ce640f9fb930c4f6eba23a
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-12-21 13:35:34 -0800

    python classes commit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -16,6 +16,7 @@ class Square:
     def size(self):
         """retrieve size of square"""
         return (self.__size)
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -23,6 +24,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
